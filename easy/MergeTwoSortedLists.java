3fde8be0677a7bdaf7a66ea13ae18c2ebcc0997d
Dias Duisembayev <64257533+dias-duisembayev@users.noreply.github.com>
2023-01-09 15:17:06 +0600

    Create MergeTwoSortedLists.java

---
+++ easy/MergeTwoSortedLists.java
@@ -0,0 +1,40 @@
+// link https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        
+        ListNode head = new ListNode();
+        ListNode current = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                current.next = list2;
+                current = current.next;
+                list2 = list2.next;
+            } else {
+                current.next = list1;
+                current = current.next;
+                list1 = list1.next;
+            }
+        }
+
+        if (list1 == null) {
+            current.next = list2;
+        } else if (list2 == null) {
+            current.next = list1;
+        }
+        
+        return head.next;
+    }
+}
