46e357c7c0d0553a3d5f3684c4fcd49fb7a4a0eb
Dias Duisembayev <64257533+dias-duisembayev@users.noreply.github.com>
2023-01-19 09:59:57 +0600

    Create ReverseLinkedListElements.java

---
+++ easy/ReverseLinkedListElements.java
@@ -0,0 +1,27 @@
+//link https://leetcode.com/problems/remove-linked-list-elements/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = new ListNode(0, head);
+        var current = temp;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return temp.next;    
+    }
+}
